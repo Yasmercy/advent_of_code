f59b7f9debdff4b4597616aabe23ae38e2d316cc
Yasmercy <ianchensoftware@gmail.com>
2023-11-04 13:31:20 -0500

    solve day18 part2

--- 2022/day18.cpp
+++ 2022/day18.cpp
@@ -5,6 +5,8 @@
 #include <unordered_set>
 #include <vector>
 
+constexpr int MAX = 25;
+
 struct Point
 {
     int x;
@@ -16,20 +18,21 @@ struct Point
         return x == other.x && y == other.y && z == other.z;
     }
 
-    std::vector<std::vector<Point>> raytrace(int n) const
+    std::vector<Point> neighbors() const
     {
-        std::vector<std::vector<Point>> out(6);
-        for (int i = 1; i <= n; ++i)
-        {
-            out[0].push_back({x + i, y, z}); // NOLINT
-            out[1].push_back({x - i, y, z}); // NOLINT
-            out[2].push_back({x, y + i, z}); // NOLINT
-            out[3].push_back({x, y - i, z}); // NOLINT
-            out[4].push_back({x, y, z + i}); // NOLINT
-            out[5].push_back({x, y, z - i}); // NOLINT
-        }
+        return {
+            {x + 1, y, z}, // NOLINT
+            {x - 1, y, z}, // NOLINT
+            {x, y + 1, z}, // NOLINT
+            {x, y - 1, z}, // NOLINT
+            {x, y, z + 1}, // NOLINT
+            {x, y, z - 1}, // NOLINT
+        };
+    }
 
-        return out;
+    int to_int() const
+    {
+        return x * MAX * MAX + y * MAX + z;
     }
 };
 
@@ -37,11 +40,56 @@ template <> struct std::hash<Point>
 {
     std::size_t operator()(const Point &p) const noexcept
     {
-        std::size_t h1 = std::hash<int>{}(p.x);
-        std::size_t h2 = std::hash<int>{}(p.y);
-        std::size_t h3 = std::hash<int>{}(p.z);
-        return h1 ^ (h2 << 2) ^ (h3 << 1);
+        return std::hash<int>{}(p.to_int());
     }
+};
+
+struct DisjointSet
+{
+    DisjointSet(int n) : ds(std::vector<int>(n, -1))
+    {
+    }
+
+    void join(int x, int y)
+    {
+        int i = find(x);
+        int j = find(y);
+
+        if (i == j)
+            return;
+
+        // union by size
+        if (size(i) < size(j))
+        {
+            ds[i] += ds[j];
+            ds[j] = i;
+        }
+        else
+        {
+            ds[j] += ds[i];
+            ds[i] = j;
+        }
+    }
+
+    int find(int x)
+    {
+        // does path compression
+        if (ds[x] >= 0)
+        {
+            int out = find(ds[x]);
+            ds[x] = out;
+            return out;
+        }
+
+        return x;
+    }
+
+    int size(int x)
+    {
+        return -ds[find(x)];
+    }
+
+    std::vector<int> ds;
 };
 
 std::unordered_set<Point> read_file(std::string filename)
@@ -69,8 +117,8 @@ int find_area(const std::unordered_set<Point> &points)
 {
     int solution = points.size() * 6;
     for (const auto &p : points)
-        for (const auto &n : p.raytrace(1))
-            if (const auto search = points.find(n[0]); search != points.end())
+        for (const auto &n : p.neighbors())
+            if (const auto search = points.find(n); search != points.end())
                 --solution;
 
     return solution;
@@ -78,37 +126,34 @@ int find_area(const std::unordered_set<Point> &points)
 
 std::unordered_set<Point> find_interior(const std::unordered_set<Point> &points)
 {
-    constexpr int max = 20;
-
-    std::unordered_set<Point> out;
-
     // find the universe set of points
     std::vector<Point> universe;
-    for (int x = 0; x < max; ++x)
-        for (int y = 0; y < max; ++y)
-            for (int z = 0; z < max; ++z)
+    for (int x = 0; x < MAX; ++x)
+        for (int y = 0; y < MAX; ++y)
+            for (int z = 0; z < MAX; ++z)
                 universe.push_back({x, y, z});
 
+    // create a disjoint set, equivalence relation = connected
+    DisjointSet ds(MAX * MAX * MAX);
+
+    // connect border with itself
+    const auto border = points.begin()->to_int();
+    for (const auto &p : points)
+        ds.join(border, p.to_int());
+
+    // connect all other points (can't pass boundary)
     for (const auto &p : universe)
-    {
-        if (const auto search = points.find(p); search != points.end())
-            continue;
+        for (const auto &n : p.neighbors())
+            if (n.to_int() >= 0 && n.to_int() < ds.ds.size() && ds.find(n.to_int()) != ds.find(border) &&
+                ds.find(p.to_int()) != ds.find(border))
+                ds.join(n.to_int(), p.to_int());
 
-        const auto rays = p.raytrace(max);
-        std::vector<bool> seen(6);
-        for (int i = 0; i < 6; ++i)
-            for (int j = 0; j < max && !seen[i]; ++j)
-                if (const auto search = points.find(rays[i][j]); search != points.end())
-                    seen[i] = true;
-
-        bool all = true;
-        for (int i = 0; i < 6 && all; ++i)
-            if (!seen[i])
-                all = false;
-
-        if (all)
+    // do a linear search to find all points that aren't on edge and boundary
+    std::unordered_set<Point> out;
+    const auto edge = universe.back().to_int();
+    for (const auto &p : universe)
+        if (ds.find(p.to_int()) != ds.find(border) && ds.find(p.to_int()) != ds.find(edge))
             out.insert(p);
-    }
 
     return out;
 }
