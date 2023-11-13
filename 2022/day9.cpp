4208ab3b96278ee681317fb7eb929d56e9510447
Yasmercy <ianchensoftware@gmail.com>
2023-11-13 16:09:53 -0600

    fix day9 part2

--- 2022/day9.cpp
+++ 2022/day9.cpp
@@ -1,108 +1,116 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <utility>
 #include <cmath>
+#include <fstream>
+#include <iostream>
 #include <set>
+#include <string>
+#include <utility>
+#include <vector>
 
-struct Move {
+struct Move
+{
     char dir;
     int n;
 };
 
-struct Location {
+struct Location
+{
     int row = 0;
     int col = 0;
 
-    void move(const Move& m) {
-        if (m.dir == 'R') {
+    void move(const Move &m)
+    {
+        if (m.dir == 'R')
             col += m.n;
-        } else if (m.dir == 'L') {
+        else if (m.dir == 'L')
             col -= m.n;
-        } else if (m.dir == 'U') {
+        else if (m.dir == 'U')
             row += m.n;
-        } else {
+        else
             row -= m.n;
-        }
     }
 
-    void follow(const Location& other) {
-        if (adjacent(other)) {
+    void follow(const Location &other)
+    {
+        if (adjacent(other))
             return;
-        }
 
-        if (row != other.row && col != other.col) {
+        if (row != other.row && col != other.col)
+        {
             // move diagonal to be 1 away
-            if (std::abs(other.row - row) > std::abs(other.col - col)) {
-                col = other.col;
-            } else {
-                row = other.row;
-            }
-            follow(other);
-        } else {
+            int dr = (other.row > row) ? 1 : -1;
+            int dc = (other.col > col) ? 1 : -1;
+            row += dr;
+            col += dc;
+        }
+        else
+        {
             // move straight to be 1 away
-            if (row == other.row) {
-                if (col < other.col) {
+            if (row == other.row)
+                if (col < other.col)
                     col = other.col - 1;
-                } else {
+                else
                     col = other.col + 1;
-                }
-            } else if (row < other.row) {
+            else if (row < other.row)
                 row = other.row - 1;
-            } else {
+            else
                 row = other.row + 1;
-            }
         }
     }
 
-    bool adjacent(const Location& other) {
+    bool adjacent(const Location &other)
+    {
         return std::abs(other.row - row) < 2 && std::abs(other.col - col) < 2;
     }
 
-    auto operator<=>(const Location& other) const = default;
+    auto operator<=>(const Location &other) const = default;
 };
 
-struct Rope {
+struct Rope
+{
     std::vector<Location> knots;
 
-    void move(const Move& m) {
+    void move(const Move &m)
+    {
         knots[0].move(m);
         // each element follows the one before
-        for (int i = 1; i < knots.size(); ++i) {
-            knots[i].follow(knots[i-1]);
-        }
+        for (int i = 1; i < knots.size(); ++i)
+            knots[i].follow(knots[i - 1]);
     }
 };
 
-std::vector<Move> read_file(std::string filename) {
+std::vector<Move> read_file(std::string filename)
+{
     std::ifstream ifs(filename);
     std::vector<Move> out;
     char dir;
     int n;
 
-    while (ifs.good()) {
+    while (ifs.good())
+    {
         ifs >> dir;
         ifs >> n;
-        if (ifs.eof()) {
+        if (ifs.eof())
             break;
-        }
-        out.push_back(Move {dir, n});
+        out.push_back(Move{dir, n});
     }
 
     return out;
 }
 
-void part_one(const std::vector<Move>& moves) {
+void part_one(const std::vector<Move> &moves)
+{
     std::set<Location> locations;
     Rope rope;
-    for (int i = 0; i < 2; ++i) {
-        rope.knots.push_back(Location {0, 0});
+    for (int i = 0; i < 2; ++i)
+    {
+        rope.knots.push_back(Location{0, 0});
     }
 
-    for (auto move : moves) {
-        for (int i = 0; i < move.n; ++i) {
-            rope.move(Move {move.dir, 1});
+    for (auto move : moves)
+    {
+        for (int i = 0; i < move.n; ++i)
+        {
+            rope.move(Move{move.dir, 1});
             locations.insert(rope.knots.back());
         }
     }
@@ -110,16 +118,21 @@ void part_one(const std::vector<Move>& moves) {
     std::cout << locations.size() << "\n";
 }
 
-void part_two(const std::vector<Move>& moves) {
+void part_two(const std::vector<Move> &moves)
+{
     std::set<Location> locations;
     Rope rope;
-    for (int i = 0; i < 10; ++i) {
-        rope.knots.push_back(Location {0, 0});
+    for (int i = 0; i < 10; ++i)
+    {
+        rope.knots.push_back(Location{0, 0});
     }
 
-    for (auto move : moves) {
-        for (int i = 0; i < move.n; ++i) {
-            rope.move(Move {move.dir, 1});
+    for (auto move : moves)
+    {
+        for (int i = 0; i < move.n; ++i)
+        {
+
+            rope.move(Move{move.dir, 1});
             locations.insert(rope.knots.back());
         }
     }
@@ -127,8 +140,8 @@ void part_two(const std::vector<Move>& moves) {
     std::cout << locations.size() << "\n";
 }
 
-
-int main() {
+int main()
+{
     std::vector<Move> moves = read_file("day9.in");
     part_one(moves);
     part_two(moves);
