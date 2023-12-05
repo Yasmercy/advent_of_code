7a196f43860c04ec1a6225df93f5b69ddcc41416
Yasmercy <ianchensoftware@gmail.com>
2023-12-05 16:04:34 -0600

    day3 with kotlin

--- 2023/day3.kt
+++ 2023/day3.kt
@@ -1,23 +1,62 @@
-fun Char.isSymbol() = !this.isDigit() && this != '.'
+import kotlin.math.abs
 
-data class Numb(val start: Int, val end: Int, val row: Int)
+val symbols = "[$&@+#=/\\-%*]".toRegex()
+val digits = "\\d+".toRegex()
 
-fun getNumbs(line: String, row: Int): List<Numb> {
-    val mask = line.map {it.isDigit()}
+data class Location(val row: Int, val col: Int)
+data class Symbol(val loc: Location, val sym: String)
 
-
-    return out
-}
-
-
-fun main() {
-    fun partOne(input: List<String>) {
-        val symMask = getSymMask(input)
-        val adjMask = getAdjMask(symMask)
-        println(getDigits(input[0], 0, adjMask))
-//        println(adjMask)
-
+fun getSymbols(input: List<String>) =
+    input.flatMapIndexed { idx, line ->
+        symbols.findAll(line).map { sym ->
+            Symbol(Location(idx, sym.range.first), sym.value)
+        }
     }
 
-    partOne(readInputs("day3.in"))
+fun getNumbers(input: List<String>) =
+    input.flatMapIndexed { idx, line ->
+        digits.findAll(line).map { n ->
+            Symbol(Location(idx, n.range.first), n.value)
+        }
+    }
+
+fun isAdjacent(a: Location, b: Location) =
+    abs(a.row - b.row) < 2 && abs(a.col - b.col) < 2
+
+fun isAdjacent(a: Location, b: Symbol) =
+    (b.loc.col..<(b.loc.col + b.sym.length)).map { col ->
+        isAdjacent(a, Location(b.loc.row, col))
+    }.any { it }
+
+fun findAdjacent(symbols: List<Symbol>, numbers: List<Symbol>) =
+    symbols.associateWith { sym ->
+        numbers.filter { n ->
+            isAdjacent(sym.loc, n)
+        }
+    }
+
+fun main() {
+    val input = readInputs("day3.in")
+    val symbols = getSymbols(input)
+    val numbers = getNumbers(input)
+    val map = findAdjacent(symbols, numbers)
+
+    fun partOne() {
+        val sum = map.values.sumOf {
+            it.sumOf { num -> num.sym.toInt() }
+        }
+        println(sum)
+    }
+
+    fun partTwo() {
+        val sum = map.filter {
+            it.key.sym == "*" && it.value.size == 2
+        }.values.sumOf {
+            it[0].sym.toInt() * it[1].sym.toInt()
+        }
+        println(sum)
+    }
+
+    partOne()
+    partTwo()
 }
