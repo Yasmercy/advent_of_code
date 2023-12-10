56ed58cdff8bf18edf4a35b0702b8caa2b493d8a
Yasmercy <ianchensoftware@gmail.com>
2023-12-10 15:19:44 -0600

    solve day10

--- 2023/day10.kt
+++ 2023/day10.kt
@@ -16,8 +16,7 @@ val tileToDir = mapOf(
 fun goToNext(start: Loc, from: Loc, map: List<List<Char>>): Loc {
     val diff = Loc(start.row - from.row, start.col - from.col)
 
-    if (!tileToDir.contains(map[start.row][start.col]))
-        return start
+    if (!tileToDir.contains(map[start.row][start.col])) return start
 
     val dir = tileToDir[map[start.row][start.col]]!!
 
@@ -41,22 +40,18 @@ fun getPath(start: Loc, dir: Loc, map: List<List<Char>>): List<Loc> {
         val tmp = cur
         cur = goToNext(cur, from, map)
         from = tmp
-        if (cur == tmp)
-            return out.toList()
+        if (cur == tmp) return out.toList()
     }
     out.add(cur)
     return out.toList()
 }
 
 fun getCycle(start: Loc, map: List<List<Char>>): List<Loc> {
-    for (dr in -1..1) {
-        for (dc in -1..1) {
-            if (abs(dr + dc % 2) != 1)
-                continue
-            val path = getPath(start, Loc(dr, dc), map)
-            if (path.size > 1 && path.first() == path.last())
-                return path
-        }
+    val dirs = listOf(Loc(-1, 0), Loc(1, 0), Loc(0, 1), Loc(0, -1))
+    for (dir in dirs) {
+        if (abs(dir.row + dir.col % 2) != 1) continue
+        val path = getPath(start, Loc(dir.row, dir.col), map)
+        if (path.size > 1 && path.first() == path.last()) return path
     }
     return listOf()
 }
@@ -64,8 +59,7 @@ fun getCycle(start: Loc, map: List<List<Char>>): List<Loc> {
 fun getStart(map: List<List<Char>>): Loc {
     for (i in map.indices) {
         for (j in map[i].indices) {
-            if (map[i][j] == 'S')
-                return Loc(i, j)
+            if (map[i][j] == 'S') return Loc(i, j)
         }
     }
     return Loc(0, 0)
@@ -111,11 +105,44 @@ fun connectAll(set: MutableList<Int>, cur: Loc, ignore: List<Int>, w: Int, h: In
     }
 }
 
+fun bufferMap(map: List<List<Char>>): List<List<Char>> {
+    val width = map[0].size
+    val height = map.size
+    val out = MutableList(height + 2) {
+        MutableList(width + 2) {
+            '.'
+        }
+    }
+    for (i in map.indices) {
+        for (j in map[0].indices) {
+            out[i + 1][j + 1] = map[i][j]
+        }
+    }
+    return out
+}
+
+fun replaceS(map: List<List<Char>>, loop: List<Loc>, start: Loc): List<List<Char>> {
+    val before = loop[1]
+    val after = loop[loop.size - 2]
+    val before_diff = Loc(start.row - before.row, start.col - before.col)
+    val after_diff = Loc(after.row - start.row, after.col - start.col)
+    val trans = Trans(before_diff, after_diff)
+
+    val m = map.toMutableList().map {it.toMutableList()}
+    for ((char, value) in tileToDir.iterator()) {
+        if (value.path1 == trans || value.path2 == trans) {
+            m[start.row][start.col] = char
+        }
+    }
+    return m
+}
 
 fun main() {
     val inputs = readInputs("day10.in").map { it.toList() }
-    val start = getStart(inputs)
-    val cycle = getCycle(start, inputs)
+    val buffed = bufferMap(inputs)
+    val start = getStart(buffed)
+    val cycle = getCycle(start, buffed)
+    val map = replaceS(buffed, cycle, start)
 
     fun partOne() {
         val sol = cycle.size / 2
@@ -123,22 +150,35 @@ fun main() {
     }
 
     fun partTwo() {
-        val height = inputs.size
-        val width = inputs[0].size
+        val height = map.size
+        val width = map[0].size
         val loop = cycle.map { node ->
             node.row * width + node.col
         }
 
         val ds = init(width * height)
-        for (node in loop)
-            union(ds, loop[0], node)
+        for (node in loop) union(ds, loop[0], node)
         for (row in 0..<height) {
             for (col in 0..<width) {
                 connectAll(ds, Loc(row, col), loop, width, height)
             }
         }
 
-        val sol = width * height - cycle.size + ds[find(ds, 0)] + 1
+        val roots = ds.mapIndexed { idx, n -> if (n < 0) idx else -1 }.filter { it >= 0}
+        val outside = mutableListOf<Int>()
+        for (y in 0..<height) {
+            var parity = 0
+            for (x in 0..<width) {
+                if (loop.contains(y * width + x) && listOf('|', 'L', 'J').contains(map[y][x]))
+                    parity += 1
+                else if (roots.contains(y * width + x) && !loop.contains(y * width + x) && parity % 2 == 0) {
+                    outside.add(y * width + x)
+                }
+            }
+        }
+
+        val keep = roots.filter {!outside.contains(it)}
+        val sol = -1 * keep.sumOf {ds[find(ds, it)]} - (loop.size - 1)
         println(sol)
     }
 
