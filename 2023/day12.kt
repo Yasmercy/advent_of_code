992202b7a4c503d26fec7ffeb72bc5eab74e5f4d
Yasmercy <ianchensoftware@gmail.com>
2023-12-13 15:31:17 -0600

    solve day12 part1

--- 2023/day12.kt
+++ 2023/day12.kt
@@ -1,41 +1,38 @@
 fun main() {
     fun solve(i: Int, k: Int, record: List<Char>, groups: List<Int>): Int {
+        // solving: amount of ways to fill the group
+        // with the kth group starting at element i
+
         // base case: index validation
-        // if there is no more input, return whether finished
-        if (i >= record.size)
-            return 0
+        if (i >= record.size || k == groups.size) return 0
 
-        // base case: no groups
-        if (k == groups.size)
-            return 0
-
-        // base case: record substring too small
+        // base case: record substring not match
         val size = groups[k]
-        if (record.size - i < size)
-            return 0
+        if (record.size - i < size || record.slice(i..<i + size).any { it == '.' }) return 0
 
-        // summing up amount of ways to create the substring
+        // summing up amount of ways to fill the remaining groups
+        // requires remaining groups to be left
         var sum = 0
-        for (start in i..<record.size - size) {
-            // ensure everything in substring is # or ?
-            if (record.slice(start..<start + size).any { it == '.' })
-                continue
-
-            // ensure spacer after (if more groups)
-            if (record[start + size] != '#')
-                sum += solve(start + size + 1, k + 1, record, groups)
-        }
-
-        // edge case where ending group is used
-        if (k == groups.size - 1 && (i..record.size - size).any { start ->
-                record.slice(start..<start + size).all { it != '.' } &&
-                        record.slice(start + size..<record.size).all { it != '#' }
+        if (k < groups.size - 1) {
+            for (start in i + size + 1..<record.size) {
+                // ensure not skipping over any hashes
+                if (record.slice(i + size..<start).any { it == '#' }) break
+                sum += solve(start, k + 1, record, groups)
             }
-        ) {
-            sum += 1
         }
 
+        if (k == groups.size - 1 && record.slice(i + size..<record.size).all { it != '#' })
+            return 1 + sum
         return sum
+    }
+
+    fun solution(record: List<Char>, groups: List<Int>): Int {
+        var lastStart = record.indexOf('#')
+        if (lastStart == -1)
+            lastStart = record.size - 1
+
+        val counts = (0..lastStart).map { solve(it, 0, record, groups) }
+        return counts.sum()
     }
 
     val inputs = readInputs("day12.in")
@@ -43,12 +40,10 @@ fun main() {
     val groups = inputs.map { line -> getAllNums(line.split(' ')[1]).map { it.toInt() } }
 
     fun partOne() {
-        val counts = records.zip(groups).map { (r, g) ->
-            solve(0, 0, r, g)
-        }
-        val sol = counts.sum()
+        val sol = records.zip(groups).sumOf { (r, g) -> solution(r, g) }
         println(sol)
     }
 
     partOne()
+
 }
