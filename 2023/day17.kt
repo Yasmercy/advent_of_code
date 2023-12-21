98643b98c465e54a8d8c7df5d7f670437dff0f12
Yasmercy <ianchensoftware@gmail.com>
2023-12-20 19:56:38 -0800

    solve day17 A*

--- 2023/day17.kt
+++ 2023/day17.kt
@@ -1,18 +1,20 @@
 import java.util.PriorityQueue
+import kotlin.math.max
 
 typealias Point = Pair<Int, Int>
 
 fun main() {
-    data class Item(val dist: Int, val loc: Point, val prev: Point, val straight: Int) : Comparable<Item> {
-        override fun compareTo(other: Item) = compareValuesBy(this, other) { it.dist }
-    }
-
-    data class HashItem(val loc: Point, val prev: Point, val straight: Int)
-
     val map = readInputs("day17.in").map { it.toList() }
     val width = map[0].size
     val height = map.size
     val end = Pair(height - 1, width - 1)
+
+    fun heuristic(cur: Point) = max((end.first - cur.first), (end.second - cur.second))
+    data class Item(val dist: Int, val loc: Point, val prev: Point, val straight: Int) : Comparable<Item> {
+        override fun compareTo(other: Item) = compareValuesBy(this, other) { it.dist + heuristic(it.loc) }
+    }
+
+    data class HashItem(val loc: Point, val prev: Point, val straight: Int)
 
     fun outBounds(cur: Point) = !inBounds(cur.first, 0, height - 1) || !inBounds(cur.second, 0, width - 1)
 
@@ -22,20 +24,14 @@ fun main() {
     fun getBack(cur: Item) =
         Point(cur.loc.first - (cur.loc.first - cur.prev.first), cur.loc.second - (cur.loc.second - cur.prev.second))
 
-    fun solve(): Int {
-        val start = Item(0, Pair(0, 0), Pair(0, 0), 0)
+    fun solve(min: Int, max: Int): Int {
+        val start = Item(0, Pair(0, 0), Pair(0, 0), min)
         val queue = PriorityQueue<Item>()
         val seen = mutableSetOf<HashItem>()
         val queued = mutableSetOf(HashItem(start.loc, start.prev, start.straight))
         queue.add(start)
 
-        var iter = 0
-
         while (!queue.isEmpty()) {
-            if (++iter % 1_000_000 == 0) {
-                println("${queue.size} ${queued.size} ${seen.size}")
-            }
-
             val cur = queue.poll()
             if (cur.loc == end) {
                 return cur.dist
@@ -54,18 +50,19 @@ fun main() {
             )
 
             for (next in search) {
-                if (outBounds(next) || next == back || (next == front && cur.straight == 2)) continue
+                if (outBounds(next) || next == back || (next == front && cur.straight == max) || (next != front && cur.straight < min)) continue
                 val weight = map[next.first][next.second] - '0'
 
                 val nextFront = Item(cur.dist + weight, next, cur.loc, cur.straight + 1)
-                val nextSide = Item(cur.dist + weight, next, cur.loc, 0)
-                if (next == front && !queued.contains(HashItem(next, cur.loc, cur.straight + 1))) {
+                val nextSide = Item(cur.dist + weight, next, cur.loc, 1)
+                val hashFront = HashItem(next, cur.loc, cur.straight + 1)
+                val hashSide = HashItem(next, cur.loc, 1)
+                if (next == front && !queued.contains(hashFront)) {
                     queue.add(nextFront)
-                    queued.add(HashItem(next, cur.loc, cur.straight + 1))
-                }
-                else if (!queued.contains(HashItem(next, cur.loc, 0))) {
+                    queued.add(hashFront)
+                } else if (!queued.contains(hashSide)) {
                     queue.add(nextSide)
-                    queued.add(HashItem(next, cur.loc, 0))
+                    queued.add(hashSide)
                 }
             }
         }
@@ -73,12 +70,16 @@ fun main() {
         return -1
     }
 
-    // fun partOne() {
-    //     val sol = solveDfs(Pair(0, 0), Pair(0, 1), 0, mutableSetOf(), 0)
-    //     println(sol)
-    // }
+    fun partOne() {
+        val sol = solve(1, 3)
+        println(sol)
+    }
 
-    // partOne()
-    val sol = solve()
-    println(sol)
+    fun partTwo() {
+        val sol = solve(4, 10)
+        println(sol)
+    }
+
+    partOne()
+    partTwo()
 }
