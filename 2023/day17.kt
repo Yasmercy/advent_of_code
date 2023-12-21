65c21aa99651f7726624521f6a6711193de8d9a7
Yasmercy <ianchensoftware@gmail.com>
2023-12-20 18:47:06 -0800

    day17 dijkstra memory outage

--- 2023/day17.kt
+++ 2023/day17.kt
@@ -1,52 +1,67 @@
+import java.util.PriorityQueue
+import kotlin.math.min
+
 typealias Point = Pair<Int, Int>
 
 fun main() {
+    data class Item(val dist: Int, val loc: Point, val prev: Point, val straight: Int) : Comparable<Item> {
+        override fun compareTo(other: Item) = compareValuesBy(this, other) { it.dist }
+        override fun equals(other: Any?) =
+            other is Item && this.loc == other.loc && this.prev == other.prev && this.straight == other.straight
+    }
+
     val map = readInputs("day17.in").map { it.toList() }
     val width = map[0].size
     val height = map.size
     val end = Pair(height - 1, width - 1)
-    var best = -1
 
     fun outBounds(cur: Point) = !inBounds(cur.first, 0, height - 1) || !inBounds(cur.second, 0, width - 1)
 
-    fun setBest(new: Int): Int {
-        if (best == -1 || new < best) best = new
-        return new
+    fun getFront(cur: Item) =
+        Point(cur.loc.first + (cur.loc.first - cur.prev.first), cur.loc.second + (cur.loc.second - cur.prev.second))
+
+    fun getBack(cur: Item) =
+        Point(cur.loc.first - (cur.loc.first - cur.prev.first), cur.loc.second - (cur.loc.second - cur.prev.second))
+
+    fun solve(): Int {
+        val queue = PriorityQueue<Item>()
+        val seen = mutableSetOf<Item>()
+        queue.add(Item(0, Pair(0, 0), Pair(0, 0), 0))
+
+        while (!queue.isEmpty()) {
+            val cur = queue.poll()
+            if (cur.loc == end) return cur.dist
+
+            if (seen.contains(cur)) continue
+            seen.add(cur)
+
+            val front = getFront(cur)
+            val back = getBack(cur)
+            val search = listOf(
+                Point(cur.loc.first + 1, cur.loc.second),
+                Point(cur.loc.first, cur.loc.second + 1),
+                Point(cur.loc.first - 1, cur.loc.second),
+                Point(cur.loc.first, cur.loc.second - 1),
+            )
+
+            for (next in search) {
+                if (outBounds(next) || next == back || (next == front && cur.straight == 2)) continue
+                val weight = map[next.first][next.second] - '0'
+
+                if (next == front) queue.add(Item(cur.dist + weight, next, cur.loc, cur.straight + 1))
+                else queue.add(Item(cur.dist + weight, next, cur.loc, 0))
+            }
+        }
+
+        return -1
     }
 
-    fun solve(cur: Point, prev: Point, count: Int, visited: MutableSet<Point>, v: Int): Int {
-        if (best != -1 && v >= best) return -1
-        if (cur == end) return setBest(v)
+    // fun partOne() {
+    //     val sol = solveDfs(Pair(0, 0), Pair(0, 1), 0, mutableSetOf(), 0)
+    //     println(sol)
+    // }
 
-        visited.add(cur)
-
-        val search = mutableListOf(
-            Pair(0, Pair(cur.first + 1, cur.second)),
-            Pair(0, Pair(cur.first, cur.second + 1)),
-            Pair(0, Pair(cur.first - 1, cur.second)),
-            Pair(0, Pair(cur.first, cur.second - 1)),
-        )
-
-        val straight = Pair(cur.first + (cur.first - prev.first), cur.second + (cur.second - prev.second))
-        search.remove(Pair(0, straight))
-        if (count < 2) search.add(Pair(count + 1, straight))
-
-        val dist = search.filterNot { (_, pos) ->
-            outBounds(pos) || visited.contains(pos)
-        }.map { (count, next) ->
-            val weight = map[next.first][next.second] - '0'
-            solve(next, cur, count, visited, v + weight)
-        }.filter { it >= 0 }
-
-        visited.remove(cur)
-
-        return if (dist.isEmpty()) -1 else dist.min()
-    }
-
-    fun partOne() {
-        val sol = solve(Pair(0, 0), Pair(0, 1), 0, mutableSetOf(), 0)
-        println(sol)
-    }
-
-    partOne()
+    // partOne()
+    val sol = solve()
+    println(sol)
 }
