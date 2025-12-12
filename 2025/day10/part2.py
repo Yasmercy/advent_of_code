946fae3f3e3b33ae8b992ae457aac6463c5f88bf
Ian Chen <ianchensoftware@gmail.com>
2025-12-11 23:09:17 -0600

    milp for day10

--- 2025/day10/part2.py
+++ 2025/day10/part2.py
@@ -1,35 +1,29 @@
 import numpy as np
+from scipy import optimize
 
 
 def search(line):
     split = line.split(" ")
-    target = [int(x) for x in split[-1].strip("\n{}").split(",")]
-    size = len(target)
+    target = np.array([int(x) for x in split[-1].strip("\n{}").split(",")])
+    m, n = len(target), len(split) - 2
 
-    buttons = []
-    for i in range(1, len(split) - 1):
-        indices = [int(x) for x in split[i].strip(")(").split(",")]
-        tmp = np.zeros(size)
-        tmp[indices] = 1
-        buttons.append(tmp)
-    buttons = np.array(buttons)
+    buttons = np.zeros((m, n), dtype=np.int32)
+    for i, button in enumerate(split[1:-1]):
+        for b in button.strip("()").split(","):
+            buttons[int(b), i] = 1
 
-    reachable = np.array([np.zeros(size)])
-    steps = 0
-    while not np.all(reachable == target, axis=1).any():
-        steps += 1
-        # print(steps, reachable.shape)
+    bounds = optimize.Bounds(0, np.inf)
+    integrality = np.array([True] * n)
+    constraints = optimize.LinearConstraint(A=buttons, lb=target, ub=target)
 
-        # numpy broadcasting to do this fast
-        pairwise = reachable[:, None, :] + buttons[None, :, :]
-        reachable = pairwise.reshape(-1, size)
-        reachable = np.unique(reachable, axis=0)
-        reachable = reachable[np.all(reachable <= target, axis=1)]
+    result = optimize.milp(
+        c=np.ones(n),
+        constraints=constraints,
+        integrality=integrality,
+        bounds=bounds,
+    )
 
-        # dist = np.min(np.max(target - reachable, axis=1))
-        # print(dist)
-
-    return steps
+    return sum(result.x)
 
 
 def main():
